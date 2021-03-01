f56006f735801802fb7952d7ac7f2d2c9c74e0b0
fancky2019 <709737588@qq.com>
2021-03-01 15:23:11 +0800

    优化完成

--- CodeGenerate/FrmSelectTabel.designer.cs
+++ CodeGenerate/FrmSelectTabel.designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CodeGenerator
 {
-    partial class FrmSelectTabel
+    partial class SelectTabelFrm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,15 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.cbSelectAll = new System.Windows.Forms.CheckBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelTableNames = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
             this.btnQuery = new System.Windows.Forms.Button();
             this.txtTableName = new System.Windows.Forms.TextBox();
@@ -45,32 +42,22 @@
             this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // cbSelectAll
             // 
-            this.button1.Location = new System.Drawing.Point(623, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "关闭";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Checked = true;
-            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Location = new System.Drawing.Point(472, 20);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(48, 16);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "全选";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.cbSelectAll.AutoSize = true;
+            this.cbSelectAll.Checked = true;
+            this.cbSelectAll.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbSelectAll.Location = new System.Drawing.Point(615, 6);
+            this.cbSelectAll.Name = "cbSelectAll";
+            this.cbSelectAll.Size = new System.Drawing.Size(48, 16);
+            this.cbSelectAll.TabIndex = 1;
+            this.cbSelectAll.Text = "全选";
+            this.cbSelectAll.UseVisualStyleBackColor = true;
+            this.cbSelectAll.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(526, 15);
+            this.button2.Location = new System.Drawing.Point(669, 1);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
@@ -78,65 +65,43 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 12);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "当前表名:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(68, 20);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 12);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "暂无";
-            // 
             // panel3
             // 
-            this.panel3.Controls.Add(this.label1);
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.Controls.Add(this.checkBox1);
+            this.panel3.Controls.Add(this.cbSelectAll);
             this.panel3.Controls.Add(this.button2);
-            this.panel3.Controls.Add(this.button1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel3.Location = new System.Drawing.Point(0, 337);
+            this.panel3.Location = new System.Drawing.Point(0, 471);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(747, 49);
+            this.panel3.Size = new System.Drawing.Size(747, 26);
             this.panel3.TabIndex = 10;
             // 
             // panel2
             // 
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 286);
+            this.panel2.Location = new System.Drawing.Point(0, 443);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(747, 51);
+            this.panel2.Size = new System.Drawing.Size(747, 28);
             this.panel2.TabIndex = 8;
             // 
             // panel4
             // 
-            this.panel4.Controls.Add(this.panel1);
+            this.panel4.Controls.Add(this.panelTableNames);
             this.panel4.Controls.Add(this.panel5);
             this.panel4.Controls.Add(this.panel2);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel4.Location = new System.Drawing.Point(0, 0);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(747, 337);
+            this.panel4.Size = new System.Drawing.Size(747, 471);
             this.panel4.TabIndex = 11;
             // 
-            // panel1
+            // panelTableNames
             // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 38);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(747, 248);
-            this.panel1.TabIndex = 10;
-            this.panel1.Tag = "is1";
+            this.panelTableNames.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelTableNames.Location = new System.Drawing.Point(0, 38);
+            this.panelTableNames.Name = "panelTableNames";
+            this.panelTableNames.Size = new System.Drawing.Size(747, 405);
+            this.panelTableNames.TabIndex = 10;
+            this.panelTableNames.Tag = "is1";
             // 
             // panel5
             // 
@@ -165,17 +130,16 @@
             this.txtTableName.Size = new System.Drawing.Size(179, 21);
             this.txtTableName.TabIndex = 0;
             // 
-            // FrmSelectTabel
+            // SelectTabelFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(747, 386);
+            this.ClientSize = new System.Drawing.Size(747, 497);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
-            this.Name = "FrmSelectTabel";
+            this.Name = "SelectTabelFrm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "请选择要生成的表";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed);
             this.Load += new System.EventHandler(this.Form2_Load);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
@@ -187,18 +151,14 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox cbSelectAll;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Button btnQuery;
         private System.Windows.Forms.TextBox txtTableName;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelTableNames;
     }
 }
